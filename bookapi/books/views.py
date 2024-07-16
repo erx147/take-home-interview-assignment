e7acee59f9c77c86d3fb8cd2e93d830e29d01394
erx147 <jcookfims@gmail.com>
2024-07-16 15:47:51 -0500

    Added books to settings.py and updated views.py

--- bookapi/books/views.py
+++ bookapi/books/views.py
@@ -1,7 +1,8 @@
 from rest_framework import viewsets
 from .models import Book
 from .serializers import BookSerializer
+from django.shortcuts import render
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
-    serializer_class = BookSerializerrom django.shortcuts import render
+    serializer_class = BookSerializer 
